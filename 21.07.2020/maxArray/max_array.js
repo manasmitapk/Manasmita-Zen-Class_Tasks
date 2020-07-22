ab208eab859ccbce821936909c84b2eef7719783
manasmitapk <67586061+manasmitapk@users.noreply.github.com>
2020-07-22 09:35:57 +0530

    Update max_array.js

--- 21.07.2020/maxArray/max_array.js
+++ 21.07.2020/maxArray/max_array.js
@@ -1,3 +1,12 @@
-var numMax = Math.max(2,5,7,4,8,9);
-console.log(numMax);
+var arr = [3,56,62,23,31,28,74];
+var max = 0;
+
+for (let ind = 0; ind<=arr.length; ind++)
+{
+	if(arr[ind] > max)
+	{
+		max = arr[ind];
+	}
+}
+console.log(max);
 
