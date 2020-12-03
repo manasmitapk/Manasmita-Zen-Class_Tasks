c3c568e5669e0c102a3bc4305a5f5456a54eef8c
manasmitapk <67586061+manasmitapk@users.noreply.github.com>
2020-12-03 16:23:13 +0530

    task nodejs-1

--- Zen-Week16/my-costum-package/add.js
+++
@@ -1,9 +0,0 @@
-let sum = function (value1, value2) {
-            return value1 + value2 ;
-};
-
-let sub = function (value1, value2) {
-    return value1 - value2 ;
-};
-
-module.exports = {sum , sub};
